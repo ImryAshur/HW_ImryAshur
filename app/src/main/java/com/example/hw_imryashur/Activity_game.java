d85fae2db9f8ff183e09d4b49ea75edda7cc4301
ASUS <Ashurimry@gmail.com>
2020-08-17 13:35:57 +0300

    Adding other activity for the winner page

--- app/src/main/java/com/example/hw_imryashur/Activity_game.java
+++ app/src/main/java/com/example/hw_imryashur/Activity_game.java
@@ -2,17 +2,15 @@ package com.example.hw_imryashur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -61,7 +59,22 @@ public class Activity_game extends AppCompatActivity {
                 .into(game_IMG_messi);
     }
 
-    // TODO: 16/08/2020 when player has no life - open new activity and post the winner
+
+    private void endGame() {
+        if (messiLife <= 0) postWinner("Ronaldo Wins!" , game_IMG_ronalno);
+        if (ronaldoLife <= 0) postWinner("Messi Wins!", game_IMG_messi);
+    }
+
+    private void postWinner(String winner , ImageView pic) {
+        pic.buildDrawingCache();
+        Bitmap bitmap = pic.getDrawingCache();
+        Intent myIntent = new Intent(Activity_game.this, Activity_menu.class);
+        myIntent.putExtra("winnerPlayer", winner); //Optional parameters
+        myIntent.putExtra("Image", bitmap);
+        Activity_game.this.startActivity(myIntent);
+        finish();
+    }
+
     private void game() {
         if (ronaldoTurn == true) {
             setButtons(game_BTN_shot_R,game_BTN_shot_M);
@@ -84,15 +97,16 @@ public class Activity_game extends AppCompatActivity {
 
     private void updateLifeColor() {
         if (messiLife <= 20 ){
-            game_progressBar_messiLife.getProgressDrawable().setColorFilter(
-                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+            lifeColor(game_progressBar_messiLife);
         }
         if (ronaldoLife <= 20){
-            game_progressBar_ronaldoLife.getProgressDrawable().setColorFilter(
-                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+            lifeColor(game_progressBar_ronaldoLife);
         }
     }
-
+    private void lifeColor(ProgressBar playerLife) {
+        playerLife.getProgressDrawable().setColorFilter(
+                Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+    }
     private void findviews() {
         game_IMG_ronalno = findViewById(R.id.game_IMG_ronaldo);
         game_IMG_messi = findViewById(R.id.game_IMG_messi);
@@ -106,6 +120,7 @@ public class Activity_game extends AppCompatActivity {
         game_BTN_penalty_M = findViewById(R.id.game_BTN_penalty_M);
     }
 
+
     private View.OnClickListener shotClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -117,6 +132,7 @@ public class Activity_game extends AppCompatActivity {
             }
             updateLifeColor();
             game();
+            endGame();
         }
     };
 
@@ -133,6 +149,7 @@ public class Activity_game extends AppCompatActivity {
             }
             updateLifeColor();
             game();
+            endGame();
         }
     };
 
@@ -147,6 +164,7 @@ public class Activity_game extends AppCompatActivity {
             }
             updateLifeColor();
             game();
+            endGame();
         }
     };
     private int setlife(int playerLife , int type , ProgressBar playerBar , boolean isRonaldo){
---
+++ app/src/main/java/com/example/hw_imryashur/Activity_menu.java
@@ -0,0 +1,53 @@
+package com.example.hw_imryashur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class Activity_menu extends AppCompatActivity {
+    private ImageView menu_IMG_winnerPic;
+    private TextView menu_LBL_winnerName;
+    private Button menu_BTN_newGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        findViews();
+        Intent intent = getIntent();
+        String winnerName = intent.getStringExtra("winnerPlayer");
+        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("Image");
+        menu_LBL_winnerName.setText(winnerName);
+        menu_BTN_newGame.setOnClickListener(newGameBtn);
+
+        Glide
+                .with(this)
+                .load(bitmap)
+                .centerCrop()
+                .into(menu_IMG_winnerPic);
+    }
+
+    private void findViews() {
+        menu_IMG_winnerPic = findViewById(R.id.menu_IMG_winnerPic);
+        menu_LBL_winnerName = findViewById(R.id.menu_LBL_winnerName);
+        menu_BTN_newGame = findViewById(R.id.menu_BTN_newGame);
+    }
+
+    private View.OnClickListener newGameBtn = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent myIntent = new Intent(Activity_menu.this, Activity_game.class);
+            Activity_menu.this.startActivity(myIntent);
+            finish();
+        }
+    };
+}
