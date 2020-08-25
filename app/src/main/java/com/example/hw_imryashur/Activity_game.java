99963fcbbce78e1654a50da7c9b31bc46216965b
ASUS <Ashurimry@gmail.com>
2020-08-25 22:12:08 +0300

    V1

--- app/src/main/java/com/example/hw_imryashur/Activity_game.java
+++ app/src/main/java/com/example/hw_imryashur/Activity_game.java
@@ -1,14 +1,12 @@
 package com.example.hw_imryashur;
-/*
- * Student - Imry Ashur
+
+/* Student - Imry Ashur
  * ID - 203958228
- * */
+ */
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
